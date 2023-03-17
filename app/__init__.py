3f3942c0e0154786dd3b60a427e29b9b85f8dc52
Okechukwu Chukwude <chukwudeokechukwu@gmail.com>
2023-03-17 15:14:52 +0100

    Restructure app config

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask
-import config
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config.from_object(config.DevelopmentConfig)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-ma = Marshmallow(db)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
--- config.py
+++ config.py
@@ -6,14 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-class ProductionConfig(Config):
-    FLASK_ENV = 'production'
-    DEBUG = False
-
-class DevelopmentConfig(Config):
-    FLASK_ENV = 'development'
-    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
